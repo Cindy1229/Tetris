4c9461bc07c732c3f6b8c511f3a3c831109fc930
Cindy1229 <leticiashi3@gmail.com>
2020-05-25 19:22:07 -0400

    Updated js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
-
+  const width=10
+  const ScoreDisplay=document.querySelector('#score')
+  const StartBtn=document.querySelector('#start-button')
+  const grid=document.querySelector('.grid')
+  let squares=Array.from(document.querySelectorAll('.grid div'))
 });
