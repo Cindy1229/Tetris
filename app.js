2926f1f05cfe0a9f794e0471e0656b7c6bb59ef2
Cindy1229 <leticiashi3@gmail.com>
2020-05-25 22:32:48 -0400

    Added all basic functionalities

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded',()=>{
   const width = 10
   let nextRandom=0
   let timerId
+  let score=0
 
 
   //Tetrominos
@@ -113,6 +114,8 @@ document.addEventListener('DOMContentLoaded',()=>{
       currentPosition=4;
       draw()
       displayShape()
+      addScore()
+      gameOver()
     }
   }
 
@@ -195,7 +198,33 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
   })
 
-  
+  //Update score
+  function addScore() {
+    for (let i=0; i<199; i+=width){
+      const row=[i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+      if (row.every(index => squares[index].classList.contains('taken'))){
+        score+=10
+        scoreDisplay.innerHTML=score
+        row.forEach(index=> {
+          squares[index].classList.remove('taken')
+          squares[index].classList.remove('tetromino')
+        })
+        const squaresRemoved=squares.splice(i, width)
+        //console.log(squaresRemoved)
+        squares=squaresRemoved.concat(squares)
+        squares.forEach((cell => grid.appendChild(cell)))
+      }
+    }
+  }
+
+  //Game over
+  function gameOver() {
+    if(current.some(index=> squares[currentPosition+index].classList.contains('taken'))) {
+      scoreDisplay.innerHTML='end'
+      clearInterval(timerId)
+    }
+  }
+
 
 
 
