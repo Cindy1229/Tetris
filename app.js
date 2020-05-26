d1618c205b89e69182b741c6dc8fc26b74d3eb8b
Cindy1229 <leticiashi3@gmail.com>
2020-05-25 21:14:06 -0400

    Implemented all movement functions

--- app.js
+++ app.js
@@ -67,6 +67,92 @@ document.addEventListener('DOMContentLoaded',()=>{
     })
   }
 
+  //Move down the tetromino every second
+  timerId=setInterval(moveDown, 1000);
+
+  //Assign listener to keys
+  function control(e) {
+    if(e.keyCode===37) {
+      //left
+      moveLeft();
+    }
+    else if (e.keyCode===38){
+      //rotate
+      rotate()
+    }
+    else if (e.keyCode===39){
+      //right
+      moveRight()
+    }
+    else if (e.keyCode===40){
+      //moveDown
+      moveDown()
+    }
+
+  }
+  document.addEventListener('keyup', control)
+
+  //Move down fucntion
+  function moveDown() {
+    undraw()
+    currentPosition+=width
+    draw()
+    freeze()
+  }
+
+  //Freeze function, check if the tetromino has hit the bottom, if does, start a new tetromino from the top
+  function freeze() {
+    if(current.some(index=> squares[currentPosition+index+width].classList.contains('taken'))){
+      current.forEach(index=>squares[currentPosition+index].classList.add('taken'))
+      //start a new tetromino
+      random=Math.floor(Math.random()*theTetrominoes.length)
+      current=theTetrominoes[random][currentRotation]
+      currentPosition=4;
+      draw()
+    }
+  }
+
+  //Move left, unless at the edge or there is a blockage
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge=current.some(index=> (currentPosition+index)%width === 0)
+
+    if(!isAtLeftEdge) currentPosition-=1
+
+    if(current.some(index=> squares[currentPosition+index].classList.contains('taken'))){
+      currentPosition+=1
+    }
+    draw()
+  }
+
+  //Move Right, unless at edge or there is a blockage
+  function moveRight() {
+    undraw()
+    const isAtRightEdge=current.some(index=>(currentPosition+index)%width===width-1)
+
+    if(!isAtRightEdge) currentPosition+=1
+
+    if(current.some(index=> squares[currentPosition+index].classList.contains('taken'))){
+      currentPosition-=1
+    }
+    draw();
+  }
+
+  //Rotate the Tetromino
+  function rotate() {
+    undraw()
+    currentRotation++
+    if (currentRotation===current.length){
+      currentRotation=0
+    }
+    current=theTetrominoes[random][currentRotation]
+    draw()
+  }
+
+
+
+
+
 
 
 
